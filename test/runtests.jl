4a0ae1dbdea0695f4cdb2e64381a7cdfb8190c54
Paul Virally <pvirally@gmail.com>
2021-10-05 11:38:14 -0700

    Fixxed typo and made the ubffer larger for better profiling

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,8 @@ using StatProfilerHTML
     # Write your tests here.
     # read_test("/Users/yukiya/Documents/Physics/PhD/ReactionNetwork.jl/moller2003.dat")
     @time read_test("/Users/pvirally/Dropbox/Waterloo/Co-op/TRIUMF/ControlFiles/wind-beta+ncap+alpha+photo.json")
-    # @profilehtml = read_test("/Users/pvirally/Dropbox/Waterloo/Co-op/TRIUMF/ControlFiles/wind-beta+ncap+alpha+photo.json")
+    # Profile.init(n=5*10^7, delay=0.005)
+    # @profilehtml read_test("/Users/pvirally/Dropbox/Waterloo/Co-op/TRIUMF/ControlFiles/wind-beta+ncap+alpha+photo.json")
     # Profile.print()
     # @test networksize == 13396
     # @test typeof(timestep)==Float64
